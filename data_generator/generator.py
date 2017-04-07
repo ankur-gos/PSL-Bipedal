bb2fe91c7613d00ff2469490007a251737dc6ab6
Ankur Goswami <agoswam3@ucsc.edu>
2017-04-07 12:55:47 -0700

    Add generator.py

---
+++ data_generator/generator.py
@@ -0,0 +1,15 @@
+'''
+    generator.py
+    Ankur Goswami, agoswam3@ucsc.edu
+    Generate dummy data
+'''
+
+import pandas as pd
+
+'''
+    generate_locations
+    generate locations for our model
+    n is the number of segments
+'''
+def generate_locations(n=20):
+    pass
