26e309e4421b9a1579d3c6b72a2247ddc06c2241
Ankur Goswami <agoswam3@ucsc.edu>
2017-04-27 13:48:41 -0700

    Add truncate_locations

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -3,3 +3,18 @@
     Preprocess locations by clustering them
     Ankur Goswami, agoswam3@ucsc.edu
 '''
+
+from sklearn.cluster import KMeans
+import numpy as np
+
+def truncate_locations(start, end):
+    with open(start, 'r') as start_file, open(end, 'r') as end_file, open('truncated_locations.txt', 'w') as fout:
+        for line in start_file:
+            # Truncate past the first two characters (segment # and tab character)
+            fout.write(line[2:])
+        for line in end_file:
+            fout.write(line[2:])
+
+def load_data(filename):
+    pass
+
