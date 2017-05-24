5382b6b7ec760d208ae83b80a5cbf0cd3e523d30
Ankur Goswami <agoswam3@ucsc.edu>
2017-05-23 17:41:11 -0700

    Add more to build script

--- build.py
+++ build.py
@@ -6,13 +6,26 @@
 
 import config
 import preprocessing.parser.parse as parser
+import preprocessing.preprocessing as preprocesser
 import subprocess
 import output.filter_truth as ft
 
 '''
-    First run through, no preprocessing needed, just raw output
+     build_cleaned_default
+     run the default pipeline using the cleaned segments
 '''
-cleaned_obs = parser.parse_cleaned_segments(config.data_path)
-parser.write_cleaned_obs(cleaned_obs, config.seg_path, config.start_loc_path, config.end_loc_path, config.start_time_path, config.end_time_path, config.mode_path)
-subprocess.call(['./run.sh'])
-ft.filter('./output/default/frequents_infer.txt', config.cleaned_frequent_results_path)
+def build_cleaned_default():
+    cleaned_obs = parser.parse_cleaned_segments(config.data_path)
+    parser.write_obs(cleaned_obs, config.seg_path, config.start_loc_path, config.end_loc_path, config.start_time_path, config.end_time_path, config.mode_path, config.segment_day_path)
+    preprocesser.run(config.start_loc_path, config.end_loc_path)
+    subprocess.call(['./run.sh'])
+    ft.filter('./output/default/frequents_infer.txt', config.cleaned_frequent_results_path)
+
+'''
+    build_constructed_default
+    run the default pipeline using the constructed segments
+'''
+def build_constructed_default():
+    pass
+
+build_cleaned_default()
